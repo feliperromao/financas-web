58c3377409543edb9c587221832c32004f7ff7c6
felipe <felipe.martins@fortics.com.br>
2021-08-13 12:11:24 -0300

    refactor: fix tailwindcss load in app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
+import 'tailwindcss/tailwind.css'
+import 'react-toastify/dist/ReactToastify.css';
 import '../styles/globals.css'
+import '../styles/nprogress.css'
+
 import type { AppProps } from 'next/app'
 
+import Router from 'next/router'
+import NProgress from 'nprogress'
+
+import  "../services/firebase";
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} ></Component>
 }
 export default MyApp
